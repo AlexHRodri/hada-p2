8b917b0bfe0266e3e8119844f41c379ed658cee9
Alejandro Hernández Rodríguez <ahr24@alu.ua.es>
2020-02-24 18:32:59 +0100

    Se añade la clase Vehiculo.cs con los atributos, getters y setters indicados por el enunciado
    48783016Q

---
+++ hada-p2/Vehiculo.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hada
+{
+    class Vehiculo
+    {
+        public static int maxVelocidad { get; set; }
+        public static int maxTemperatura { get; set; }
+        public static int minCombustible { get; set; }
+        
+        public static Random rand { private get; set; }
+
+        public string nombre { get; private set; }
+
+        private int velocidad
+        {
+            get
+            {
+                return velocidad;
+            }
+
+            set
+            {
+                if(value > maxVelocidad || value < 0)
+                {
+                    if(value > maxVelocidad)
+                    {
+
+                    }
+                    else
+                    {
+                        velocidad = 0;
+                    }
+                }
+                else
+                {
+                    velocidad = value;
+                }
+            }
+        }
+        private int temperatura
+        {
+            get
+            {
+                return temperatura;
+            }
+
+            set
+            {
+                if (value > maxTemperatura)
+                {
+
+                }
+                else
+                {
+                    temperatura = value;
+                }
+            }
+        }
+        private int combustible
+        {
+            get
+            {
+                return velocidad;
+            }
+
+            set
+            {
+                if(value < minCombustible || value < 0 || value > 100)
+                {
+                    if(value < minCombustible)
+                    {
+
+                    }
+                    else if (value < 0)
+                    {
+                        combustible = 0;
+                    }
+                    else
+                    {
+                        combustible = 100;
+                    }
+                }
+                else
+                {
+                    combustible = value;
+                }
+            }
+        }
+
+
+
+
+
+    }
+}
